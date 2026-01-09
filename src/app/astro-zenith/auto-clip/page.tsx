f8aa904ff97bb235aa24b7fd5f87654087aa36f9
Fendrik Nurul Jadid <fendrik1311@gmail.com>
2026-01-09 13:36:06 +0700

    Update websocket

--- src/app/astro-zenith/auto-clip/page.tsx
+++ src/app/astro-zenith/auto-clip/page.tsx
@@ -277,7 +277,7 @@ export default function Page() {
 
   const subscribeProgress = (videoId: string) => {
     const ws = new WebSocket(
-      `ws://localhost:2003/ws/progress?video_id=${videoId}`
+      `ws://ws-zenith.astrovia.id/ws/progress?video_id=${videoId}`
     )
   
     ws.onmessage = async (e) => {
